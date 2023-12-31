11feb71fedcebe64509672b7f63502f1daced4cd
gbrllcavichion <cavichionmichel@gmail.com>
2023-12-31 00:04:49 -0300

    :sparkles: Creating getAllInvestments

--- src/main/java/api/myfinancial/service/investment/InvestmentService.java
+++ src/main/java/api/myfinancial/service/investment/InvestmentService.java
@@ -1,4 +1,9 @@
 package api.myfinancial.service.investment;
 
+import api.myfinancial.dto.*;
+
+import java.util.*;
+
 public interface InvestmentService {
+    List<Investment> getAllInvestments(String userId);
 }
--- src/main/java/api/myfinancial/service/investment/InvestmentServiceImpl.java
+++ src/main/java/api/myfinancial/service/investment/InvestmentServiceImpl.java
@@ -1,10 +1,32 @@
 package api.myfinancial.service.investment;
 
+import api.myfinancial.domain.*;
+import api.myfinancial.dto.*;
 import lombok.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+
+import java.util.*;
 
 @AllArgsConstructor
 @Service
 public class InvestmentServiceImpl implements InvestmentService {
+
+    @Autowired
+    private final InvestmentRepository investmentRepository;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Override
+    public List<Investment> getAllInvestments(String userId) {
+        User user = userRepository.findById(userId).orElse(null);
+
+        if (user != null) {
+            return user.getInvestments();
+        } else {
+            throw new IllegalArgumentException("User not found");
+        }
+    }
 
 }
